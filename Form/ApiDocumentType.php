1f9a24bd5f83f3118d609d796c85bdf0c746d808
Brahim BOUKOUFALLAH <boukoufallah.brahim@gmail.com>
2018-03-09 15:49:49 +0100

    Add api routes for documents.

---
+++ Form/ApiDocumentType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace IDCI\Bundle\DocumentManagementBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use IDCI\Bundle\DocumentManagementBundle\Form\EventListener\DocumentTransformEventSubscriber;
+use IDCI\Bundle\DocumentManagementBundle\Model\Document;
+
+class ApiDocumentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('reference')
+            ->add('description', TextareaType::class)
+            ->add('data');
+
+        $builder->addEventSubscriber(new DocumentTransformEventSubscriber());
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Document::class,
+        ]);
+    }
+}
