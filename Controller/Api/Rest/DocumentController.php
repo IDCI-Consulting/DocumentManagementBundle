b1b5340e3c80c23071728bad5b231f80351567b2
Eddie Barraco <contact@eddiebarraco.fr>
2018-10-05 09:07:18 +0200

    Multiple document fetch fild parameters:
    - Allow to use null fields to fetch all
    - Allow to filter by name, reference and templateId
    - Forgoten values != empty values
    "?templateId=&name=toto" != "?name=toto"

--- Controller/Api/Rest/DocumentController.php
+++ Controller/Api/Rest/DocumentController.php
@@ -28,6 +28,8 @@ class DocumentController extends FOSRestController
      * Retrieve a set of documents.
      *
      * @QueryParam(name="reference", nullable=true, description="(optional) Reference")
+     * @QueryParam(name="name", nullable=true, description="(optional) Name")
+     * @QueryParam(name="templateId", nullable=true, description="(optional) Template id")
      *
      * @param string reference
      *
@@ -35,8 +37,19 @@ class DocumentController extends FOSRestController
      */
     public function getDocumentsAction(ParamFetcher $paramFetcher)
     {
+        $criteria = [];
+        if (null !== $paramFetcher->get('reference')) {
+            $criteria['reference'] = $paramFetcher->get('reference');
+        }
+        if (null !== $paramFetcher->get('name')) {
+            $criteria['name'] = $paramFetcher->get('name');
+        }
+        if (null !== $paramFetcher->get('templateId')) {
+            $criteria['templateId'] = $paramFetcher->get('templateId');
+        }
+
         $view = $this->view(
-            $this->getDoctrine()->getManager()->getRepository(Document::class)->findBy($paramFetcher->all()),
+            $this->getDoctrine()->getManager()->getRepository(Document::class)->findBy($criteria),
             Response::HTTP_OK
         );
 
